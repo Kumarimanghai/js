f8e3ed6479dfa2da5dc5bb4600a3267f4222c54b
kumarimanghai <manghai972004@gmail.com>
2026-06-12 00:16:08 +0530

    new para added

--- text.js
+++ text.js
@@ -1,3 +1,4 @@
 <script>
-    nothing
+    nothing is impossible to study in coding as software developer beacuse
+     siva is creating and directing path i need to walk
 </script>
--- txt2.js
+++ txt2.js
@@ -1,3 +1,6 @@
 <script>
     hello vanakkam yaam iruka bayamen by murugan
+
+
+    hello i am learning github murugan is teaching me
 </script>
